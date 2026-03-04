b064882e5b3547f0f5d7e4ff3389f50eeaff8316
Ivigon <ivi.silvacarvalho@gmail.com>
2026-03-04 11:58:33 -0300

    Add welcome message script in Node.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 
+// Um pequeno script em Node.js para dar as boas-vindas
+const usuario = "Ivanir";
+const tecnologia = "Node.js";
+
+console.log(`Olá, ${usuario}! Seu ambiente ${tecnologia} está configurado com sucesso.`);
+console.log("Este é o primeiro passo da minha jornada Full Stack.");
